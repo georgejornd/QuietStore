e612568c350438f6117e8fda8f52d944b62b52d2
gerratresher <georgejornd@mail.ru>
2022-11-06 07:34:47 +0300

    product list variant & pagination

--- src/components/product/Stars.tsx
+++ src/components/product/Stars.tsx
@@ -1,17 +1,19 @@
 import React from "react";
+import {prepareStyleProperty} from "../../utils/prepare-style-property";
 
 type StarsFill = "primary" | "secondary"
 
 interface StarsProps {
     rating: number
     fill: StarsFill
+    top?: number
 }
 
-const Stars: React.FC<StarsProps> = ({rating, fill}) => {
+const Stars: React.FC<StarsProps> = ({rating, fill, top}) => {
     const width = ((rating * 10) / 0.5) + "%"
 
     return (
-        <div className="stars stars-background">
+        <div className="stars stars-background" style={{marginTop: prepareStyleProperty("px", top)}}>
             <div className={`stars stars-line stars-line-${fill}`} style={{width}}></div>
         </div>
     )
