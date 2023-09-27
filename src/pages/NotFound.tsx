cde9caf45e478e251ef561203d8ffea92daca3af
georgejornd <georgejornd@gmail.com>
2023-09-27 04:08:04 +0300

    prettier

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,1 +1,5 @@
-const NotFound = () => {    return <div>Page not found | 404 | </div>}export default NotFound
+const NotFound = () => {
+    return <div>Page not found | 404 | </div>
+}
+
+export default NotFound
